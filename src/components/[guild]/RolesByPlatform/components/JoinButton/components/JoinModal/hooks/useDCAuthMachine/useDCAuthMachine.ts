b968126770a5ae94e7bcf6a911f8dc1a0698034a
BrickheadJohnny <johnny.zgen@gmail.com>
2022-03-16 22:16:33 +0100

    Using "_blank" for the popup target

--- src/components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/hooks/useDCAuthMachine/useDCAuthMachine.ts
+++ src/components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/hooks/useDCAuthMachine/useDCAuthMachine.ts
@@ -88,7 +88,7 @@ const useDCAuthMachine = (): Machine<ContextType> => {
       openWindow: () => {
         authWindow.current = window.open(
           `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&response_type=token&scope=identify&redirect_uri=${process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI}&state=${guild?.urlName}`,
-          "dc_auth",
+          "_blank",
           `height=750,width=600,scrollbars`
         )
 
